8559b578f260ce6c66c5a0a356a04ed4d596bc74
imhphari <imhphari@gmail.com>
2018-08-29 18:53:18 +0530

    alpha 1.3.0

--- js/manual.js
+++ js/manual.js
@@ -1,10 +1,6 @@
 (function($) {
   "use strict"; 
 
-	$(window).scroll(function(){
-    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
-  //250 is fade pixels
-  });
  
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
